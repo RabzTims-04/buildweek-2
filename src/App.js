fb80176d5f21f5aba2b67007068fb86ee229a175
hasham zafar <hasham307@gmail.com>
2021-06-14 16:39:38 +0200

    margin fixed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 
-import { BrowserRouter as Router,Route  } from 'react-router-dom'
+
 
 
 import './App.css';
