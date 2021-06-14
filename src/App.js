966f2195d6e312b3002151b2ca9d344b4aa740e4
hasham zafar <hasham307@gmail.com>
2021-06-14 14:44:29 +0200

    navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router,  } from 'react-router-dom'
 import './App.css';
-
+import Head from './components/Navbar';
 function App() {
   return (
     <div>
+       
       <Router>
-
+         <Head/>
       </Router>
       
     </div>
