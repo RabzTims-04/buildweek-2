7862f4a6959f1f74930b1695b218475d3f95f834
Carl Dawkins <carlanthony782@gmail.com>
2021-06-14 13:37:23 +0000

    Revert "Carl"

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './App.css';
-import Profile from "./components/Profile";
 
 function App() {
   return (
     <div>
       <Router>
-
-        <Route exact path="/" component={Profile}/>
 
       </Router>
       
