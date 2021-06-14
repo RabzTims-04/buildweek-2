60acbf4a357b453175a9e48485308288eee33788
Rabz92 <rabbiailyas92@gmail.com>
2021-06-14 15:12:37 +0200

    Profile left card1
    Profile leftcard1 created and fetched information, Modal created for editing the intro. On the way for Method:PUT

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -5,17 +5,50 @@ import ProfileCardOne from './ProfileCardOne';
 
 class Profile extends Component {
 
+    state={
+        profileData:[]
+    }
+
+    componentDidMount =()=>{
+        this.fetchData()
+    }
+
+    fetchData = async ()=>{
+        try {
+            const url = 'https://striveschool-api.herokuapp.com/api/profile/me'
+            const key= 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MGM3MjIzMzI5MTkzMDAwMTU2MGFiYTEiLCJpYXQiOjE2MjM2NjMxNTYsImV4cCI6MTYyNDg3Mjc1Nn0.pHCHEeBWoL8ouo2bml9H3Ju13WPbylVyEqIpyeFhx1o'
+
+            const response = await fetch(url,{
+                headers:{
+                    'Authorization': key
+                }
+            })
+            const data = await response.json()
+            if(response.ok){
+                console.log(data); 
+                this.setState({
+                    profileData:data
+                })               
+            }
+            
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     render() {
 
         return (
            <Container fluid>
 
-               <Row>
-                   <Col md={8}>
-                       <ProfileCardOne/>
+               <Row className="justify-content-between">
+                   <Col md={8} className="p-5">
+                       <ProfileCardOne profileData={this.state.profileData}/>
                    </Col>
 
                    <Col md={4}>
+                       {/* Carls Components */}
+
                    </Col>
                </Row>
 
