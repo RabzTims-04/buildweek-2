9f7b273c9d83c12e83bad9127e87efa02b7c67b6
hasham zafar <hasham307@gmail.com>
2021-06-14 15:56:19 +0200

    ....

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-<<<<<<< HEAD
+
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-=======
+
 
 import { BrowserRouter as Router,Route  } from 'react-router-dom'
 
->>>>>>> 55823561e9d2f91ccae53295f621fb1a8c4550af
+
 import './App.css';
 import Profile from "./components/Profile";
 import Head from "./components/Navbar";
