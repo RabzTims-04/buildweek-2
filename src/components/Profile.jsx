50563af519a687a9a787e4029ca6c48b28630c48
Rabz92 <rabbiailyas92@gmail.com>
2021-06-15 10:20:13 +0200

    Final Day-1

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,12 +1,15 @@
 import { Component } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import '../css/Profile.css';
-//Components
 import ProfileCardOne from './ProfileCardOne';
 import ProfileStrength from './ProfileStrength';
 import RProfileCardOne from './RProfileCardOne';
 import RProfileCardTwo from './RProfileCardTwo';
 import YourDashboard from './YourDashboard';
+import PeopleAlsoViewed from './PeopleAlsoViewed';
+import Learning from './Learning';
+/* import Dashboard from './Dashboard'; */
+import Messaging from './Messaging';
 
 class Profile extends Component {
 
@@ -64,7 +67,7 @@ class Profile extends Component {
            <Container fluid>
 
                <Row className="justify-content-between profilePage">
-                   <Col md={9} className="p-5 d-flex flex-column">
+                   <Col md={8} className="pt-5 d-flex flex-column">
                        <div>
                          <ProfileCardOne editInfo={this.editInfo} profileData={this.state.profileData}/>
                        </div>
@@ -75,76 +78,25 @@ class Profile extends Component {
 
                        <div className="mt-3">
                          <YourDashboard/>
+                         {/* <Dashboard /> */}
                        </div>
                     
                    </Col>
 
-                   <Col md={3}>
-                       <RProfileCardOne/>
-                       <RProfileCardTwo/>
+                   <Col md={4}>
+                       {/* <RProfileCardOne/>
+                       <RProfileCardTwo/> */}
+                       <PeopleAlsoViewed />
+                       <Learning />
                        {/* Carls Components */}
 
                    </Col>
                </Row>
+               <Messaging />
 
            </Container>
         );
-
-import PeopleAlsoViewed from './PeopleAlsoViewed';
-import Learning from './Learning';
-import Dashboard from './Dashboard';
-import Messaging from './Messaging';
-class Profile extends Component {
-  state = {
-    profileData: [],
-  };
-
-  componentDidMount = () => {
-    this.fetchData();
-  };
-
-  fetchData = async () => {
-    try {
-      const url = 'https://striveschool-api.herokuapp.com/api/profile/me';
-      const key =
-        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MGM3MjIzMzI5MTkzMDAwMTU2MGFiYTEiLCJpYXQiOjE2MjM2NjMxNTYsImV4cCI6MTYyNDg3Mjc1Nn0.pHCHEeBWoL8ouo2bml9H3Ju13WPbylVyEqIpyeFhx1o';
-
-      const response = await fetch(url, {
-        headers: {
-          Authorization: key,
-        },
-      });
-      const data = await response.json();
-      if (response.ok) {
-        console.log(data);
-        this.setState({
-          profileData: data,
-        });
-      }
-    } catch (error) {
-      console.log(error);
-
     }
-  };
-
-  render() {
-    return (
-      <Container fluid>
-        <Row className='justify-content-between'>
-          <Col md={8} className='p-5'>
-            <ProfileCardOne profileData={this.state.profileData} />
-            <Dashboard />
-          </Col>
-
-          <Col md={4}>
-            <PeopleAlsoViewed />
-            <Learning />
-          </Col>
-        </Row>
-        <Messaging />
-      </Container>
-    );
-  }
 }
 
 export default Profile;
--- src/components/ProfileCardOne.jsx
+++ src/components/ProfileCardOne.jsx
@@ -7,7 +7,9 @@ import '../css/ProfileCardOne.css';
 import background from '../assets/backgroundCover.png';
 
 class ProfileCardOne extends Component {
+
   render() {
+
     return (
       <Card className='rounded'>
         <Card.Img className='cardImg' variant='top' src={background} />
@@ -34,93 +36,20 @@ class ProfileCardOne extends Component {
                 <p>19 connections</p>
               </Link>
 
-
+                            </Col>
                             <Col className="text-right" md={4}>
 
                                <EditModal profileData={this.props.profileData} editInfo={this.props.editInfo}/>
+                               </Col>
                                
-                            </Col>
+                        
                         </Row>
-                       {/*  <Row>
-                            <Col lg={6}>
-                                <Card.Footer className="text-muted d-flex">
-                                    <Link style={{lineHeight:'1.2em'}}>
-                                      
-                                            <span className="d-block text-dark"><strong>Show recruiters you're open to work</strong> - you control who sees this.</span>
-                                            <span>Get Started</span>
-                                        
-                                    </Link>
-                                </Card.Footer>
-                            </Col>
 
-                            <Col  lg={6}>
-                                <Card.Footer className="text-muted d-flex">
-                                    <Link style={{lineHeight:'1.2em'}}>
-                                      
-                                            <span className="d-block text-dark"><strong>Share that you're hiring</strong> and attract qualified cadidates.</span>
-                                            <span>Get Started</span>
-                                        
-                                    </Link>
-                                </Card.Footer>
-                            </Col>
-                        </Row> */}
-                    
-//                     </Card.Body>
-//                 </Card>
-         
-//         );
-//     }
+                    </Card.Body>
+                </Card>
+                 );
+                 }
 
-//               <div className='d-inline'>
-//                 <Button className='badge-pill btn1' variant='primary'>
-//                   Open to
-//                 </Button>
-//                 <Button
-//                   className='badge-pill mx-2 btn2'
-//                   variant='outline-secondary'
-//                 >
-//                   Add section
-//                 </Button>
-//                 <Button className='badge-pill btn3' variant='outline-secondary'>
-//                   More
-//                 </Button>
-//               </div>
-//             </Col>
-
-//             <Col md={4}>
-//               <EditModal />
-//             </Col>
-//           </Row>
-//           <Row>
-//             <Col lg={6}>
-//               <Card.Footer className='text-muted d-flex'>
-//                 <Link style={{ lineHeight: '1.2em' }}>
-//                   <span className='d-block text-dark'>
-//                     <strong>Show recruiters you're open to work</strong> - you
-//                     control who sees this.
-//                   </span>
-//                   <span>Get Started</span>
-//                 </Link>
-//               </Card.Footer>
-//             </Col>
-
-//             <Col lg={6}>
-//               <Card.Footer className='text-muted d-flex'>
-//                 <Link style={{ lineHeight: '1.2em' }}>
-//                   <span className='d-block text-dark'>
-//                     <strong>Share that you're hiring</strong> and attract
-//                     qualified cadidates.
-//                   </span>
-//                   <span>Get Started</span>
-//                 </Link>
-//               </Card.Footer>
-//             </Col>
-//           </Row>
-        </Card.Body>
-      </Card>
-    );
-  }
-
-}
+                }
 
 export default ProfileCardOne;
