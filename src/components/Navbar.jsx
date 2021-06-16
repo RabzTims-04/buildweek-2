b4bab63e4b1bc63a215dcdc7b48a69f8f47e1d4a
Rabz92 <rabbiailyas92@gmail.com>
2021-06-16 10:49:32 +0200

    Home Newsfeed
    Basic structure

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { Navbar, Nav, NavDropdown, Form, FormControl } from 'react-bootstrap';
+import {Link, withRouter} from 'react-router-dom'
 import '../css/Head.css';
 
 const Head = () => {
@@ -18,7 +19,7 @@ const Head = () => {
       <Navbar.Toggle aria-controls='basic-navbar-nav' />
       <Navbar.Collapse id='basic-navbar-nav'>
         <Nav className='ml-auto'>
-          <Nav.Link href='#home' className='menuText'>
+          <Link to="/feed" className='menuText nav-link'>
             <svg
               xmlns='http://www.w3.org/2000/svg'
               width='16'
@@ -30,7 +31,7 @@ const Head = () => {
               <path d='M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5z' />
             </svg>
             Home
-          </Nav.Link>
+          </Link>
           <Nav.Link href='#home' className='menuText'>
             <svg
               xmlns='http://www.w3.org/2000/svg'
@@ -157,4 +158,4 @@ const Head = () => {
     </Navbar>
   );
 };
-export default Head;
+export default withRouter(Head);
