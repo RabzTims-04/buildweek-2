426c65a37c889e120860e680d893e67b5cdc93dc
hasham zafar <hasham307@gmail.com>
2021-06-16 15:56:44 +0200

    card start

---
+++ src/components/HomeLeft.jsx
@@ -0,0 +1,19 @@
+
+
+
+
+import { Component } from 'react';
+
+class HomeLeft =()=> {
+
+    
+
+        return (
+
+            <div>
+
+            </div>
+        );
+    
+}
+export default HomeLeft;
