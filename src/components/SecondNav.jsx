edfd80af0c804892e326abe86e2ce5a4e3fc6e23
Rabz92 <rabbiailyas92@gmail.com>
2021-06-16 09:29:05 +0200

    minor updates

--- src/components/SecondNav.jsx
+++ src/components/SecondNav.jsx
@@ -20,7 +20,7 @@ const SecondNav =()=>{
         <Form inline id="container">
             <InputGroup>
             <InputGroup.Prepend>
-            <svg id='avatar'xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+            <svg id='avatar'xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
                 <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
                 <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
                 </svg>  
@@ -38,7 +38,7 @@ const SecondNav =()=>{
             
             <Button id="more"type="submit" style={{borderRadius:"25px",border:'1px solid black',marginLeft:'10px',marginRight:'10px'}}>More</Button>
             <Button id="addsection"type="submit"style={{borderRadius:"25px",border:'1px solid black',marginLeft:'10px',marginRight:'10px'}}>Add section</Button>
-            <Button  classNames="btn btn-primary"type="submit"style={{borderRadius:"25px",marginLeft:'10px',marginRight:'10px'}} >Open to</Button>
+            <Button  className="btn btn-primary"type="submit"style={{borderRadius:"25px",marginLeft:'10px',marginRight:'10px'}} >Open to</Button>
         </Form>
         
         </Navbar>
