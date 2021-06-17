681fd462d6f77df7e8d6a11fd3e0496d33e787db
Rabz92 <rabbiailyas92@gmail.com>
2021-06-18 00:29:39 +0200

    profile photo posted

--- src/components/ProfileCardOne.jsx
+++ src/components/ProfileCardOne.jsx
@@ -7,18 +7,82 @@ import '../css/ProfileCardOne.css';
 import background from '../assets/backgroundCover.png';
 
 class ProfileCardOne extends Component {
+
+  state={
+    profPic:'',
+    myProfile:null
+  }
+
+  postCardProfileImg = async (e)=>{
+    let formData = new FormData()   
+    formData.set('profile', this.state.myProfile.image)
+    e.preventDefault()
+    const url = 'https://striveschool-api.herokuapp.com/api/profile/me/picture'
+    const key = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MGM3MjIzMzI5MTkzMDAwMTU2MGFiYTEiLCJpYXQiOjE2MjM2NjMxNTYsImV4cCI6MTYyNDg3Mjc1Nn0.pHCHEeBWoL8ouo2bml9H3Ju13WPbylVyEqIpyeFhx1o'
+    try {
+
+      const response = await fetch(url,{
+        method:'POST',
+        body: formData,
+        headers:{
+          Authorization:key,
+         /*  'content-type': 'application/json' */
+        }
+      })
+      const data = await response.json()
+      console.log(data);
+      const img = await data.image
+      console.log(img);
+      /* this.props.image(img) */
+      if(response.ok){
+        alert('pic posted')
+        this.setState({
+          ...this.state,
+          profPic:img
+        })
+       
+        
+      }
+      else{
+        console.log('lets try again');
+      }
+      
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   render() {
+
     return (
       <Card className='rounded'>
         <Card.Img className='cardImg' variant='top' src={background} />
-
+        
 
                          <Link to="/">
-                         <AiFillCamera className="camerabtn" style={{color:'blue', position:'absolute', right:'4%', top:'4%'}} size={20}/></Link>
+                         <AiFillCamera onClick={(e)=>{this.postCardProfileImg(e)}} className="camerabtn" style={{color:'blue', position:'absolute', right:'4%', top:'4%'}} size={20}/></Link>
                     
                     <Card.Body> 
-                                            
-                        <img className="profilePic img-fluid" src={this.props.profileData.image} alt="profile-pic"/> 
+                    <label className="p-0 d-flex m-0" for="postimg">
+                    <img className="profilePic img-fluid" src={this.state.profPic?this.state.profPic:this.props.profileData.image} alt="profile-pic"/> 
+                                </label>
+                                <input 
+                                    /* onClick={(e)=> {e.stopPropagation()
+                                    return true}} */
+                                    style={{display:'none'}}
+                                    type="file"
+                                    title="choose"
+                                    id="postimg"
+                                /* id="image" */
+                                    onChange={(e) => {this.setState({
+                                      ...this.state,
+                                      myProfile:{...this.state.myProfile, 
+                                            image: e.target.files[0]}
+                                })
+                                console.log(e.target.files[0])}}
+                                
+                                
+                                />
                                          
                         <Row className='mb-4 justify-content-between'>
                             <Col md={8} className="d-flex flex-column cardBody mt-4">
