bb7f38cede14e6c59ff7e448088db3a071c183f3
Rabz92 <rabbiailyas92@gmail.com>
2021-06-18 20:20:54 +0200

    time date function added

--- src/components/NewsFeed.jsx
+++ src/components/NewsFeed.jsx
@@ -89,6 +89,20 @@ class NewsFeed extends Component {
         }
     }
 
+    ageFunc =(dataDate) =>{
+        /* let today = new Date('2021-02-13T00:00:00.000Z')
+        let date = today.getDate()
+        let month = today.getMonth()
+        let year = today.getFullYear()
+        console.log('fulldate',today);
+        console.log('date', date);
+        console.log('month', month);
+        console.log('year', year); */
+        let date = new Date(dataDate).toLocaleDateString("sq-AL",{ year: 'numeric', month: '2-digit', day: '2-digit' })
+        console.log('date',date);
+        return date
+    }
+
     render() {
 
         return (
@@ -105,7 +119,7 @@ class NewsFeed extends Component {
                             <Link to=""><p className="p-0 m-0">{news.user.name} {news.user.surname}</p></Link>
                             <span style={{fontSize:'0.7em'}} className="text-muted">{news.user.title}</span>
                             <div className="d-flex flex-row">
-                                <span style={{fontSize:'0.7em'}} className="text-muted">{news.user.createdAt}</span>
+                                <span style={{fontSize:'0.7em'}} className="text-muted">{this.ageFunc(news.user.createdAt)}</span>
                                 <svg style={{height:'14px', width:'14px'}} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" data-supported-dps="12x12" fill="currentColor" className="mercado-match m-0 p-0 ml-2" width="12" height="12" focusable="false">
                                 <path d="M8 1a7 7 0 107 7 7 7 0 00-7-7zM3 8a5 5 0 011-3l.55.55A1.5 1.5 0 015 6.62v1.07a.75.75 0 00.22.53l.56.56a.75.75 0 00.53.22H7v.69a.75.75 0 00.22.53l.56.56a.75.75 0 01.22.53V13a5 5 0 01-5-5zm6.24 4.83l2-2.46a.75.75 0 00.09-.8l-.58-1.16A.76.76 0 0010 8H7v-.19a.51.51 0 01.28-.45l.38-.19a.74.74 0 01.68 0L9 7.5l.38-.7a1 1 0 00.12-.48v-.85a.78.78 0 01.21-.53l1.07-1.09a5 5 0 01-1.54 9z"></path>
                                 </svg>
